b298b6c3295cafec851d3c5a884d284dc4d817ae
xtansy <xtansy@mail.ru>
2023-03-30 16:15:01 +0300

    some fix

--- src/redux/dialogSlice/selectors.ts
+++ src/redux/dialogSlice/selectors.ts
@@ -4,8 +4,8 @@ export const dialogsSliceSelector = (state: RootState) => state.dialogSlice;
 
 export const dialogsSelector = (state: RootState) => state.dialogSlice.dialogs;
 
-export const dialogsLoadingSelector = (state: RootState) => state.dialogSlice.isLoading;
+export const dialogsLoadingSelector = (state: RootState) =>
+    state.dialogSlice.isLoading;
 
-export const dialogsErrorSelector = (state: RootState) => state.dialogSlice.isError;
-
-
+export const dialogsErrorSelector = (state: RootState) =>
+    state.dialogSlice.errorMessage;
