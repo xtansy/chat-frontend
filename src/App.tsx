ad33b08d113ed9df067199a6db42d805c89d8067
xtansy <xtansy@mail.ru>
2023-03-31 16:13:48 +0300

    nado

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@ import { Routes, Route } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 import { ROUTES } from "@utils/constants";
-import { AuthPage, HomePage, ProfilePage, ChangeInfoPage, ChangePasswordPage, NotFoundPage } from "@pages";
+import {
+    AuthPage,
+    HomePage,
+    ProfilePage,
+    ChangeInfoPage,
+    ChangePasswordPage,
+    NotFoundPage,
+} from "@pages";
 import { userIsAuthSelector } from "@redux/userSlice/selectors";
 import { useInitial } from "@utils/api/hooks";
 
 const App = () => {
-
     useInitial();
 
     const isAuth = useSelector(userIsAuthSelector);
@@ -15,17 +21,26 @@ const App = () => {
     return (
         <>
             <Routes>
+                {isAuth ? (
+                    <>
+                        <Route path={ROUTES.HOME} element={<HomePage />} />
+                        <Route
+                            path={ROUTES.PROFILE}
+                            element={<ProfilePage />}
+                        />
+                        <Route
+                            path={ROUTES.CHANGE_INFO}
+                            element={<ChangeInfoPage />}
+                        />
+                        <Route
+                            path={ROUTES.CHANGE_PASSWORD}
+                            element={<ChangePasswordPage />}
+                        />
+                    </>
+                ) : (
+                    <Route path={ROUTES.AUTH} element={<AuthPage />} />
+                )}
                 <Route path={"*"} element={<NotFoundPage />} />
-                {
-                    isAuth ?
-                        <>
-                            <Route path={ROUTES.HOME} element={<HomePage />} />
-                            <Route path={ROUTES.PROFILE} element={<ProfilePage />} />
-                            <Route path={ROUTES.CHANGE_INFO} element={<ChangeInfoPage />} />
-                            <Route path={ROUTES.CHANGE_PASSWORD} element={<ChangePasswordPage />} />
-                        </>
-                        : <Route path={ROUTES.AUTH} element={<AuthPage />} />
-                }
             </Routes>
         </>
     );
