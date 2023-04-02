fd0cad86bcbf0a4a133cf19534e2dcc059ac17fb
xtansy <xtansy@mail.ru>
2023-04-02 21:58:28 +0300

    change url's for working in localhost

--- src/utils/api/requests/instance.ts
+++ src/utils/api/requests/instance.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
+// http://localhost:8080/
+// https://chat-backend-production-4414.up.railway.app/
 
 const api = axios.create({
-    baseURL: "https://chat-backend-production-4414.up.railway.app/",
+    baseURL: "http://localhost:8080/",
 });
 
 export default api;
--- src/utils/socket/index.ts
+++ src/utils/socket/index.ts
@@ -1,5 +1,6 @@
 import { io, Socket } from "socket.io-client";
-
+// http://localhost:8080/
+// https://chat-backend-production-4414.up.railway.app/
 export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
-    "https://chat-backend-production-4414.up.railway.app/"
+    "http://localhost:8080/"
 );
