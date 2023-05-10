022f6d90d90f6fcbf1bb53c5a0bb929839f571cd
Andrew <skid.one@inbox.ru>
2023-05-10 20:47:08 +0300

    for deploy10

--- src/utils/api/requests/instance.ts
+++ src/utils/api/requests/instance.ts
@@ -2,10 +2,7 @@ import axios from "axios";
 // http://localhost:8080/
 // https://chat-backend-nu.vercel.app
 
-console.log("process", process);
-console.log("process.env", process.env);
-
-const URL = process.env.REACT_APP_URL || "http://localhost:8080/";
+const URL = process.env.VITE_URL || "http://localhost:8080/";
 
 
 
