8fd5dbdae6fdd65453b5208989c2f7258a09d08e
Andrew <skid.one@inbox.ru>
2023-05-10 20:57:37 +0300

    for deploy11

--- src/utils/api/requests/instance.ts
+++ src/utils/api/requests/instance.ts
@@ -2,9 +2,8 @@ import axios from "axios";
 // http://localhost:8080/
 // https://chat-backend-nu.vercel.app
 
-const URL = process.env.VITE_URL || "http://localhost:8080/";
-
-
+const BACKEND_URL = import.meta.env.VITE_URL;
+const URL = BACKEND_URL || "http://localhost:8080/";
 
 const api = axios.create({
     baseURL: URL,
