d5bd1bd2c9fc3dd8a97db6dc7a164e0cfb4a38c3
Andrew <skid.one@inbox.ru>
2023-05-10 20:25:55 +0300

    for deploy5

--- src/utils/api/requests/instance.ts
+++ src/utils/api/requests/instance.ts
@@ -1,11 +1,9 @@
-import dotenv from "dotenv";
 import axios from "axios";
-dotenv.config();
 // http://localhost:8080/
 // https://chat-backend-nu.vercel.app
 
 const api = axios.create({
-    baseURL: process.env.BACKEND_URL || "http://localhost:8080/",
+    baseURL: process.env.REACT_APP_BACKEND_URL || "http://localhost:8080/",
 });
 
 export default api;
